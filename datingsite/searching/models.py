58a3ab0957a3855c9e999f9ba3dd6105ef9d86ba
argecyplusplus <tema.chernyuk@gmail.com>
2023-10-10 21:18:27 +0300

    Промежутка анкеты

--- datingsite/searching/models.py
+++ datingsite/searching/models.py
@@ -3,17 +3,20 @@ from django.db import models
 # Create your models here.
 class Form(models.Model):
     genders = (
-        ('М', 'Мужчина'),
-        ('Ж', 'Женщина'),
-        ('Г', 'Гей'),
-        ('Л', 'Лесбиянка'), 
-        ('С', 'Сковородкасексуал')
+        ('М', 'Парень'),
+        ('Ж', 'Девушка')
+    )
+    searching_for_list = (
+        ('М', 'Парня'),
+        ('Ж', 'Девушку'),
+        ('В', 'Кого угодно')
     )
 
     name = models.CharField ('Имя', max_length=50)
+    avatar = models.ImageField ('Фото', default='https://www.meme-arsenal.com/memes/9deabcb50a53c324b3a4981528215040.jpg')
     age = models.IntegerField("Возраст")
-    gender = models.CharField ("Пол", choices=genders, max_length=1) #если true то мужчина
-
+    gender = models.CharField ("Пол", choices=genders, max_length=1, default=genders[0])
+    searching_for = models.CharField ("Ищу", choices=searching_for_list, max_length=1, default=searching_for_list[1])
     description = models.TextField ('О себе', max_length=50)
     #... можно расширить
 
