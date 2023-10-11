d22171327123b3341cca7d8d4cae8e2bcf61aed6
argecyplusplus <tema.chernyuk@gmail.com>
2023-10-11 19:10:21 +0300

    Update models.py

--- datingsite/searching/models.py
+++ datingsite/searching/models.py
@@ -11,12 +11,21 @@ class Form(models.Model):
         ('Ж', 'Девушку'),
         ('В', 'Кого угодно')
     )
+    point_of_searching_list = (
+        ('О', 'Отношения'),
+        ('С', 'Свидания'),
+        ('Д', 'Дружба'),
+        ('', 'Общение+
+        '),
+    )
 
     name = models.CharField ('Имя', max_length=50)
     avatar = models.ImageField ('Фото', default='https://www.meme-arsenal.com/memes/9deabcb50a53c324b3a4981528215040.jpg')
     age = models.IntegerField("Возраст")
     gender = models.CharField ("Пол", choices=genders, max_length=1, default=genders[0])
     searching_for = models.CharField ("Ищу", choices=searching_for_list, max_length=1, default=searching_for_list[1])
+    city = models.CharField ('Город', max_length=100)
+    point_of_searching = models.CharField ("Цель знакомства", choices=point_of_searching_list, max_length=1, default=point_of_searching_list[0])
     description = models.TextField ('О себе', max_length=50)
     #... можно расширить
 
