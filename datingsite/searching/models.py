eaf704088cf45a71489d6a6251dd0c7fb9bf6230
argecyplusplus <tema.chernyuk@gmail.com>
2023-10-26 20:38:05 +0300

    миграции прошли успешно

--- datingsite/searching/models.py
+++ datingsite/searching/models.py
@@ -21,16 +21,13 @@ class Profile(models.Model):
     point_of_searching = models.CharField ("Цель знакомства", choices=point_of_searching_list, max_length=30, default=point_of_searching_list[0])
     city = models.CharField ('Город', max_length=100, default="Не указано")
     description = models.TextField ('О себе', max_length=50)
-    social = models.TextField ('СсылкаНаСоцсеть', max_length=50, default='@name')
-    #user = models.ForeignKey(User, verbose_name='АвторАнкеты', on_delete=models.CASCADE, default='admin')
-
+    social = models.CharField ('Ссылка на соцсеть', max_length=50, default='@name')
+    user = models.ForeignKey(User, verbose_name='Автор анкеты', on_delete=models.CASCADE, default=1)
+    
     class Meta:
         verbose_name = "Анкета"
         verbose_name_plural = "Анкеты"
 
     def __str__(self):
         return f'{self.name}'
-
-class MyProfile(models.Model):
-    pass
 
