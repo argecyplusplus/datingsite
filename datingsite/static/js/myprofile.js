440122beb3643b195ac1f761d359714b5eff6258
Mordahaoff <artemiymorev2016@gmail.com>
2023-12-14 13:09:13 +0300

    update myprofile.js

--- datingsite/static/js/myprofile.js
+++ datingsite/static/js/myprofile.js
@@ -37,32 +37,3 @@ function validateForm(event) {
     event.preventDefault();
   }
 }
-
-
-// function validateForm() {
-//   var minv = document.getElementById('min').value;
-//   var maxv = document.getElementById('max').value;
-
-//   // Проверить значения полей формы
-//   if (minv > maxv) {
-//     alert('Введите имя');
-//     return false; // предотвратить отправку формы
-//   }
-
-//   return true; // разрешить отправку формы, если все значения проходят проверку
-// }
-
-// var form = document.getElementById('myForm');
-
-// form.addEventListener('submit', function(event) {
-//   // Отменить отправку формы по умолчанию
-//   event.preventDefault();
-
-//   // Запустить функцию проверки значений
-//   var isValid = validateForm();
-
-//   if (isValid) {
-//     // Если значения проходят проверку, можно отправить форму
-//     form.submit();
-//   }
-// });
